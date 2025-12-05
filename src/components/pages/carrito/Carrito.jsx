25bf261d152777fa347069f4fe7fd43044712500
Luis Robles <luisgeremiasrobles@gmail.com>
2025-12-04 21:46:59 -0300

    Agregue cards con inputs en la pagina del carrito, tambien agregue una ruta para llegar a ella

---
+++ src/components/pages/carrito/Carrito.jsx
@@ -0,0 +1,170 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const Carrito = () => {
+    return (
+        <section className="container-fluid mt-3">
+            <Form>
+                <div className="row">
+
+                    {/* Columna para tomar los datos del usuario */}
+                    <div className="col-12 col-md-6 border border-black">
+                        <div className="card mb-4 shadow-sm">
+                            <div className="card-body">
+                                <h5 className='text-center'>Datos del cliente</h5>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Nombre</Form.Label>
+                                            <Form.Control type="text" placeholder="Ej: Juan" />
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Apellido</Form.Label>
+                                            <Form.Control type="text" placeholder="Ej: Perez" />
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-md-6"><Form.Group className="mb-3">
+                                        <Form.Label>DNI</Form.Label>
+                                        <Form.Control type="text" placeholder="Ej: 45785688" />
+                                        <Form.Text className="text-muted">
+                                            We'll never share your email with anyone else.
+                                        </Form.Text>
+                                    </Form.Group>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Email</Form.Label>
+                                            <Form.Control type="email" placeholder="Ej: juanperez@gmail.com" />
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Telefono</Form.Label>
+                                            <Form.Control type="text" placeholder="Ej: 3863457824" />
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card mb-4 shadow-sm">
+                            {/* Datos del envio */}
+                            <div className="card-body">
+                                <h5>Datos de envio</h5>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Telefono</Form.Label>
+                                            <Form.Select>
+                                                <option value="">Seleccione una provincia</option>
+                                                <option value="Buenos Aires">Buenos Aires</option>
+                                                <option value="Cordoba">Cordoba</option>
+                                                <option value="Neuquen">Neuquen</option>
+                                                <option value="Tucuman">Tucuman</option>
+                                                <option value="Salta">Salta</option>
+                                            </Form.Select>
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Ciudad</Form.Label>
+                                            <Form.Control type='text' placeholder='ej: Concepcion' />
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-12">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Telefono</Form.Label>
+                                            <Form.Control type='text' placeholder='ej: Calle Elm Street' />
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Codigo Postal</Form.Label>
+                                            <Form.Control type='text' placeholder='ej: 7895' />
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="card mb-4 shadow-sm">
+                            {/* Datos del metodo de pago */}
+                            <div className="card-body">
+                                <h5>Metodo de pago</h5>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Tarjeta</Form.Label>
+                                            <Form.Select>
+                                                <option value="">Seleccione su tarjeta</option>
+                                                <option value="MasterCard">MasterCard</option>
+                                                <option value="Visa">Visa</option>
+                                                <option value="Narana">Naranja</option>
+                                            </Form.Select>
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Numero Tarjeta</Form.Label>
+                                            <Form.Control type="text" placeholder="Ej: 7852 4556 7893" />
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Cuotas</Form.Label>
+                                            <Form.Select>
+                                                <option value="">Cantidad a pagar</option>
+                                                <option value="3">3 Cuotas sin interes</option>
+                                                <option value="6">6 Cuotas sin interes</option>
+                                                <option value="12">12 Cuotas sin interes</option>
+                                            </Form.Select>
+                                            <Form.Text className="text-muted">
+                                                We'll never share your email with anyone else.
+                                            </Form.Text>
+                                        </Form.Group>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 border border-black">1234</div>
+                </div>
+            </Form>
+        </section >
+
+    );
+};
+
+export default Carrito;
