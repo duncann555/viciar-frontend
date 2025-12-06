da794c2460fdde37b846514243005c05327819a5
Eduardo Velazques <eduwavee@gmail.com>
2025-12-06 13:50:21 -0300

    termine la pagina de registro con validaciones y estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home.jsx";
+import Register from "./components/pages/Register.jsx";
 
 function App() {
-
   return (
-    <>
-      <h1>HOLA</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontSize: "32px" }}>
+      <h1>HOLA</h1>
+    </div>
+  );
+}
