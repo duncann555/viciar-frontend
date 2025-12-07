ea5480170c0d8104156234dd330f60c4afc3a341
MaxiGT <mazisecundario@gmail.com>
2025-12-07 01:09:09 -0300

    agregamos el navbar del proyecto

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,31 +1,48 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { NavLink, Link } from 'react-router';
-import '../../styles/menu.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink, Link, Form } from "react-router";
+import "../../styles/menu.css";
+import logoS2 from "../../assets/logoS2.png";
 
 function Menu() {
   return (
-    <Navbar expand="lg" className="navbar-bg-color">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar expand="lg" className="navbar-bg-color py-lg-3 py-md-2">
+      <Container className="position-relative">
+        <Navbar.Brand href="/" className="pb-2">
+          <img src={logoS2} alt="Logo" className="img-navbar" />
+        </Navbar.Brand>
+        <div className="d-flex flex-grow-1 justify-content-center">
+          <form className="d-lg-flex ms-auto me-4">
+            <input
+              type="search"
+              placeholder="Buscar..."
+              className="form-control me-2 barra-busqueda barra-small"
+              aria-label="Search"
+            />
+          </form>
+        </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink to="/">Inicio</NavLink>
-            <NavLink to= "/admin">Admin</NavLink>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
+        <Navbar.Collapse id="basic-navbar-nav ">
+          <Nav className="ms-auto text-center">
+            <NavLink to="/" className="nav-link opcion-nav fw-bold">
+              Inicio
+            </NavLink>
+            <NavLink to="/admin" className="nav-link opcion-nav fw-bold">
+              Admin
+            </NavLink>
+            <NavLink to="/" className="nav-link d-none opcion-nav fw-bold">
+              Pedidos
+            </NavLink>
+            <NavLink to="/" className="nav-link d-none opcion-nav fw-bold">
+              Usuarios
+            </NavLink>
+            <NavLink to="/" className="nav-link d-none opcion-nav fw-bold">
+              <i class="bi bi-cart3"></i>
+            </NavLink>
+            <NavLink to="/" className="nav-link opcion-nav fw-bold">
+              <i class="bi bi-person-circle icono-user"></i>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
