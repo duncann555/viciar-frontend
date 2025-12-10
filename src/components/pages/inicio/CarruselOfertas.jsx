b9a9de78a20a735f260ec4aa026cbf2282a13a41
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-12-10 17:02:40 -0300

    cambie las imagenes del carrusel de promociones

--- src/components/pages/inicio/CarruselOfertas.jsx
+++ src/components/pages/inicio/CarruselOfertas.jsx
@@ -3,23 +3,22 @@ import ps2 from "../../../assets/ps2.png";
 import pc from "../../../assets/pc.png";
 import silla from "../../../assets/SILLA.mp4";
 import videoblack from "../../../assets/videoblack.mp4";
+import joystick from "../../../assets/Joystick.mp4";
+import juego from "../../../assets/juego.mp4";
+import xbox from "../../../assets/xbox.mp4";
 
 const CarruselOferta = () => {
     return (
         <Carousel>
             <CarouselItem>
-                <img loading="eager" className="imagen-ofertas" src={ps2} alt="oferta ps2" />
+                <video autoPlay muted loop playsInline className="imagen-ofertas" src={joystick} alt="video de blackFriday" />
+            </CarouselItem>
+            <CarouselItem className="">
+                <video autoPlay muted loop playsInline className="imagen-ofertas" src={juego} alt="video oferta silla gamer" />
             </CarouselItem>
             <CarouselItem>
-                <video autoPlay muted loop playsInline className="imagen-ofertas" src={videoblack} alt="video de blackFriday" />
+                <video autoPlay muted loop playsInline className="imagen-ofertas" src={xbox} alt="video oferta silla gamer" />
             </CarouselItem>
-            <CarouselItem>
-                <img loading="eager" className="imagen-ofertas" src={pc} alt="oferta pc" />
-            </CarouselItem>
-            <CarouselItem>
-                <video autoPlay muted loop playsInline className="imagen-ofertas" src={silla} alt="video oferta silla gamer" />
-            </CarouselItem>
-            
         </Carousel>
         
     );
