c9f60ee08eef191ad091e9aeee228e3fb4645195
MaxiGT <mazisecundario@gmail.com>
2025-12-16 17:15:38 -0300

    modificamos el navbar para mostrar los botones segun el usuario logueado. unimos la pagina de registro con el backend. agregamos la logica del CRUD a la tabla de usuarios del admin. agregamos la logica para que el numero de usuarios registrados este en paralelo con la cantidad de usuarios de la tabla del admin

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,6 +1,11 @@
 const usuariosBackend = import.meta.env.VITE_API_USUARIOS;
 const productosBackend = import.meta.env.VITE_API_PRODUCTOS;
 
+const obtenerToken = () => {
+  const usuarioLocal = JSON.parse(sessionStorage.getItem("usuarioKey")); 
+  return usuarioLocal?.token || "";
+};
+
 export const login = async (usuario) => {
   try {
     const respuesta = await fetch(usuariosBackend + "/login", {
@@ -14,6 +19,87 @@ export const login = async (usuario) => {
   } catch (err) {
     console.log(err);
     return null;
+  }
+};
+
+export const leerUsuarios = async () => {
+  try {
+    const respuesta = await fetch(usuariosBackend, {
+      method: "GET",
+      headers: {
+        "x-token": obtenerToken(), 
+      },
+    });
+
+    const datos = await respuesta.json()
+    return datos;
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const crearUsuario = async (usuario) => {
+  try {
+    const respuesta = await fetch(usuariosBackend, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": obtenerToken(),
+      },
+      body: JSON.stringify(usuario),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const editarUsuario = async (usuario, id) => {
+  try {
+    const respuesta = await fetch(`${usuariosBackend}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": obtenerToken(),
+      },
+      body: JSON.stringify(usuario),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const cambiarEstadoUsuario = async (id, nuevoEstado) => {
+  try {
+    const respuesta = await fetch(`${usuariosBackend}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": obtenerToken(),
+      },
+      body: JSON.stringify({
+        estado: nuevoEstado,
+      }),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const borrarUsuario = async (id) => {
+  try {
+    const respuesta = await fetch(`${usuariosBackend}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "x-token": obtenerToken(),
+      },
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
   }
 };
 
