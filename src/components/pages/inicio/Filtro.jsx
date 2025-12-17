28b4c5de7200e8523d6aee50d453236f83bda6e0
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-12-16 23:37:54 -0300

    modifique la consulta a la api usando los helpers declarados y agregue breadcrumb para la navegacion en el componente filtro

--- src/components/pages/inicio/Filtro.jsx
+++ src/components/pages/inicio/Filtro.jsx
@@ -1,7 +1,8 @@
 import CardProducto from "./CardProducto.jsx";
 import { Col, Container, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router";
+import { useSearchParams, Link } from "react-router";
+import { obtenerProductoNombreAPI} from "../../../helpers/queries.js";
 
 const Filtro = () => {
   const [productos, setProductos] = useState([]);
@@ -12,11 +13,14 @@ const Filtro = () => {
 
     const obtenerProductos = async () => {
       try {
-        const respuesta = await fetch(
-          `http://localhost:3000/api/productos/buscar?nombre=${busqueda}`
-        );
-        const data = await respuesta.json();
-        setProductos(data);
+        const respuesta = await obtenerProductoNombreAPI(busqueda);
+       if (respuesta && respuesta.status === 200) {
+          const data = await respuesta.json();
+          const productosActivos = data.filter((p) => p.estado === "Activo");
+          setProductos(productosActivos);
+        } else {
+          setProductos([]); 
+        }
       } catch (error) {
         console.error("Error trayendo productos:", error);
       }
@@ -29,14 +33,24 @@ const Filtro = () => {
 
     <Container className="pb-5">
       <div className="mb-5">
-        <h1 className="py-3 text-center fw-bolder text-primary-emphasis">
+        <nav aria-label="breadcrumb" className="mt-3">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/" className="text-decoration-none">Inicio</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Resultados de búsqueda
+          </li>
+        </ol>
+      </nav>
+        <h1 className="text-center fw-bolder text-primary-emphasis">
           Resultados encontrados para: {searchParams.get("nombre") || "Todos los productos"}
         </h1>
       </div>
       <Row>
         {productos.length > 0 ? (
           productos.map((producto) => (
-            <CardProducto key={producto.id} producto={producto} />))
+            <CardProducto key={producto._id} producto={producto} />))
         ) : (
           <h4 className="text-center fw-light">No se encontraron productos con ese nombre.</h4>
         )}
